a0a5d917520502ea2c1c34d6326def6fe7f00a72
Ying Jun <wandy1208@live.com>
2018-01-24 16:18:48 +0800

    fork and init

---
+++ demo/tests/demo.robot
@@ -0,0 +1,39 @@
+*** Settings ***
+Suite Setup       Start webapp and open browser
+Suite Teardown    Stop webapp and close all browsers
+Variables         ../resources/config.py    # this is the only place where we have to hard-code a path;    # when config.py is loaded it will alter the path to include    # the resources folder.
+Library           PageObjectLibrary
+Library           Selenium2Library
+Library           Process
+
+*** Variables ***
+${BROWSER}        chrome
+
+*** Test Cases ***
+Login smoke test
+    [Setup]    Go to page    LoginPage
+    Login as a normal user
+    The current page should be    HomePage
+
+Login with valid credentials
+    [Setup]    Go to page    LoginPage
+    Enter username    Demo User
+    Enter password    password
+    Click the submit button
+    The current page should be    HomePage
+
+Login with invalid credentials
+    [Setup]    Go to page    LoginPage
+    Enter username    Demo User
+    Enter password    bogus password
+    Click the submit button
+    The current page should be    LoginPage
+
+*** Keywords ***
+Stop webapp and close all browsers
+    Terminate all processes
+    Close all browsers
+
+Start webapp and open browser
+    start process    python    ${CONFIG.demo_root}/webapp/demoserver.py
+    open browser    ${CONFIG.root_url}    ${BROWSER}
---
+++ setup.py
@@ -0,0 +1,34 @@
+# N.B. to push a new version to PyPi, update the version number
+# in rfhub/version.py and then run 'python setup.py sdist upload'
+from setuptools import setup
+
+execfile('PageObject/version.py')
+
+setup(
+    name             = 'robotframework-pageobject',
+    version          = __version__,
+    author           = 'Ying Jun',
+    author_email     = 'wandy1208@gmail.com',
+    url              = 'https://github.com/WandyYing/robotframework-pageobject',
+    keywords         = 'robotframework',
+    license          = 'Apache License 2.0',
+    description      = 'RobotFramework library that implements the Page Object pattern',
+    long_description = open('README.md').read(),
+    zip_safe         = True,
+    include_package_data = True,
+    install_requires = ['robotframework', 'robotframework-seleniumlibrary', 'selenium', 'six'],
+    classifiers      = [
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+        "Framework :: Robot Framework",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Testing",
+        "Topic :: Software Development :: Quality Assurance",
+        "Intended Audience :: Developers",
+        ],
+    packages         =[
+        'PageObject',
+    ],
+    scripts          =[], 
+)
